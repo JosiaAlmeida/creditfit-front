708b97951d476fc2aae09bff479326d41818de3c
Josia Almeida <josiaalmeida0001@gmail.com>
2024-06-04 19:25:25 +0100

    build: adicionando rel no link para estilo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
       link: [
         {
           href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+          rel: "stylesheet",
         },
       ],
       script: [
