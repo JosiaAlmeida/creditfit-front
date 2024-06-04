6a82c1b396b87aa9dd629ae5e2ad0ba5035a4e98
Josia Almeida <josiaalmeida0001@gmail.com>
2024-06-04 23:15:24 +0100

    style: criando e personalizando step 2

---
+++ components/steps/two.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <p>Divididas em:</p>
+    <div class="d-flex flex-wrap gap-3">
+      <CardTerm v-for="i in 4" :key="i" :selected="i == 2" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,31 @@
 const amount = ref<number>(10000);
 const step = ref(0);
 
-const infoText = [
-  "Você possui saldo para Crédito Consignado pela empresa Seguros Seguradora. Faça uma simulação! Digite quanto você precisa:",
-  "Escolha a opção de parcelamento que melhor funcionar para você:",
-  "Pronto! Agora você já pode solicitar o empréstimo e recebê-lo na sua Conta Credifit! Veja o resumo da simulação!",
-  "Você solicitou seu empréstimo! Agora aguarde as etapas de análises serem concluídas!",
+const infoStep = [
+  {
+    infoBox:
+      "Você possui saldo para Crédito Consignado pela empresa Seguros Seguradora. Faça uma simulação! Digite quanto você precisa:",
+    button: "Simular empréstimo",
+  },
+  {
+    infoBox: "Escolha a opção de parcelamento que melhor funcionar para você:",
+    button: "Seguinte",
+  },
+  {
+    infoBox:
+      "Pronto! Agora você já pode solicitar o empréstimo e recebê-lo na sua Conta Credifit! Veja o resumo da simulação!",
+    button: "Solicitar empréstimo",
+  },
+  {
+    infoBox:
+      "Você solicitou seu empréstimo! Agora aguarde as etapas de análises serem concluídas!",
+    button: "Novo empréstimo",
+  },
 ];
 
 const handleStep = (value: number) => {
   const element = step.value + 1;
-  step.value = element > infoText.length - 1 ? infoText.length - 1 : element;
+  step.value = element > infoStep.length - 1 ? infoStep.length - 1 : element;
 };
 
 const lessStep = () => {
@@ -26,14 +41,14 @@ const setAmount = (value: number) => {
 
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row mb-5">
       <form @submit.prevent="handleStep(1)" class="col-md-6 mt-4 mx-auto">
         <BreadCrumb />
         <div class="mt-4">
           <Card class-card="shadow border-0 rounded-3">
             <div class="card-body pb-5">
               <h6 class="text-global-primary fw-bold">Simular Empréstimo</h6>
-              <BoxInfo class="mt-3" :text="infoText[step]" />
+              <BoxInfo class="mt-3" :text="infoStep[step].infoBox" />
               <div class="my-4 d-flex justify-content-center mx-auto">
                 <h5
                   class="fw-bold text-nowrap bg-global-primary-10 px-3 py-2 rounded-5 text-global-primary-2"
@@ -60,8 +75,10 @@ const setAmount = (value: number) => {
               class-button="btn-global-outline-primary rounded-5 px-5 btn py-1 bg-white"
             />
             <FormButton
-              text="Simular empréstimo"
-              class-button="btn-global-primary rounded-5 px-3 py-1 btn"
+              :text="infoStep[step].button"
+              :class-button="`btn-global-primary rounded-5  py-1 btn ${
+                step == 1 ? 'px-5' : 'px-3'
+              }`"
             />
           </div>
         </div>
