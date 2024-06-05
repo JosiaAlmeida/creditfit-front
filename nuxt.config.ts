9bc0997221f4369be7a7e4b972ae5a59abef9df8
Josia Almeida <josiaalmeida0001@gmail.com>
2024-06-05 16:43:14 +0100

    feat: adicionando empresa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  devServer: { port: 3000 },
+  devServer: { port: 3001 },
   app: {
     head: {
       link: [
